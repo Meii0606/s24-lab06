ccb368e9008971256c0ff8da6fbeed6748a5920f
Yimei <yimeiw@andrew.cmu.edu>
2024-02-21 18:14:17 -0500

    Task 1 add isOccupaied & isValid

--- Java/src/main/java/frogger/Road.java
+++ Java/src/main/java/frogger/Road.java
@@ -12,6 +12,27 @@ public class Road {
         this.occupied = occupied;
     }
 
+    /**
+     * Checks if the position is occupied.
+     *
+     * @param position position to check
+     * @return true if the position is occupied, else false.
+     */
+    public boolean isOccupied(int position) {
+        return this.occupied[position];
+    }
+
+    /**
+     * Checks if the position is valid.
+     *
+     * @param position position to check
+     * @return true if the position is valid, else false.
+     */
+    public boolean isValid(int position) {
+        if (position < 0) return false;
+        return position < this.occupied.length;
+    }
+
     public boolean[] getOccupied() {
         return this.occupied;
     }
